661e4b6a58f0e70c1ea592376ac1f693556ddbf5
BarakoLlama <65786758+BarakoLlama@users.noreply.github.com>
2020-05-31 16:25:39 -0500

    Implemented support for HTML
    Probably the most useful thing if you want anything to work.

--- main.js
+++ main.js
@@ -10,26 +10,30 @@ const rl = readline.createInterface({
 })
 var http = require('http')
 var {listenPort} = require('./config.json')
+var supportedFileTypes = (JSON.parse(fs.readFileSync("./assets/supportedFileTypes.json"))).data
 console.log("Started".brightGreen)
 
 http.createServer(function (req, res) {
-    console.log(("Responding ".brightGreen)+req.url)
+    console.log(("RESPONDING ".brightGreen)+req.url)
     let noError = true
+    // index.html
     try {
-        var readDir = fs.readdirSync(("./html"+req.url))
+        if(!req.url.includes(".")){
+            var readDir = fs.readdirSync(("./html"+req.url))
+        }
     }catch(e){
         if(e){
             noError = false
             if(e.message.includes("no such file")){
                 let notFound = e.message.split("'")[1].replace("./html", "")
-                console.log(("WARNING: Invalid request given: "+notFound).brightYellow)
+                console.log("WARNING ".brightYellow+"Invalid request given: "+notFound)
                 res.writeHead(404, {'Content-Type':'text/html'})
                 res.write(fs.readFileSync("./assets/404.html"))
                 res.end()
             }
         }
     }
-    if(noError){
+    if(noError && !req.url.includes(".")){
         if(readDir.includes("index.html")){
             res.writeHead(200, {"Content-Type":"text/html"})
             res.write(fs.readFileSync(("./html"+req.url+"/index.html")))
@@ -37,6 +41,38 @@ http.createServer(function (req, res) {
         }else{
             res.writeHead(404, {"Content-Type":"text/html"})
             res.write(fs.readFileSync("./assets/404.html"))
+            res.end()
+        }
+    }
+    // any.html
+    if(!res.writableEnded && req.url.endsWith(".html")){
+        noError = true
+        try {
+            var file = fs.readFileSync(("./html"+req.url))
+        }catch(e){
+            if(e){
+                if(e.message.includes("no such file")){
+                    noError = false
+                    let notFound = e.message.split("'")[1].replace("./html", "")
+                    console.log("WARNING ".brightYellow+"Invalid request given: "+notFound)
+                    res.writeHead(404, {'Content-Type':'text/html'})
+                    res.write(fs.readFileSync("./assets/404.html"))
+                    res.end()
+                }
+            }
+        }
+        if(noError){
+            res.writeHead(200, {"Content-Type":"text/html"})
+            res.write(fs.readFileSync(("./html"+req.url)))
+            res.end()
+        }
+    }
+    // Check for unsupported file type
+    if(!res.writableEnded && req.url.includes(".")){
+        let fileType = req.url.split(".")[1]
+        if(!supportedFileTypes.includes(fileType)){
+            res.writeHead(501, {"Content-Type":"text/html"})
+            res.write(fs.readFileSync("./assets/unsupportedFileType.html"))
             res.end()
         }
     }
