0d8b4b38d88dbe5c565977adf4926f0605ec97db
BarakoLlama <65786758+BarakoLlama@users.noreply.github.com>
2020-05-31 15:52:17 -0500

    Started HTML finder
    Now finds the proper HTML file for a requested directory

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const keypress = require('keypress')
 const request = require('request')
 const readline = require('readline')
 const colors = require('colors')
+const fs = require('fs')
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -12,10 +13,33 @@ var {listenPort} = require('./config.json')
 console.log("Started".brightGreen)
 
 http.createServer(function (req, res) {
-    console.log("Responded to a request".brightGreen)
-    res.writeHead(200, {'Content-Type':'text/html'})
-    res.write("Hello World!\n"+req.url)
-    res.end()
+    console.log(("Responding ".brightGreen)+req.url)
+    let noError = true
+    try {
+        var readDir = fs.readdirSync(("./html"+req.url))
+    }catch(e){
+        if(e){
+            noError = false
+            if(e.message.includes("no such file")){
+                let notFound = e.message.split("'")[1].replace("./html", "")
+                console.log(("WARNING: Invalid request given: "+notFound).brightYellow)
+                res.writeHead(404, {'Content-Type':'text/html'})
+                res.write(fs.readFileSync("./assets/404.html"))
+                res.end()
+            }
+        }
+    }
+    if(noError){
+        if(readDir.includes("index.html")){
+            res.writeHead(200, {"Content-Type":"text/html"})
+            res.write(fs.readFileSync(("./html"+req.url+"/index.html")))
+            res.end()
+        }else{
+            res.writeHead(404, {"Content-Type":"text/html"})
+            res.write(fs.readFileSync("./assets/404.html"))
+            res.end()
+        }
+    }
 }).listen(listenPort)
 
 keypress(process.stdin)
