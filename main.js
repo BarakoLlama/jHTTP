fb23d23c5f685553393732dce03f1bd768cc37cd
BarakoLlama <65786758+BarakoLlama@users.noreply.github.com>
2020-06-01 03:55:20 -0500

    Added support for mp4 files
    Finally, not only video, but audio too!

--- main.js
+++ main.js
@@ -423,6 +423,50 @@ http.createServer(function (req, res) {
             res.end()
         }
     }
+    // any.mp4
+    if(!res.writableEnded && req.url.endsWith(".mp4")){
+        noError = true
+        try {
+            var file = fs.readFileSync("./html"+req.url)
+        }catch(e){
+            if(e){
+                if(e.message.includes("no such file")){
+                    noError = false
+                    res.writeHead(404, {'Content-Type':'text/html'})
+                    res.write(fs.readFileSync("./assets/404.html"))
+                    res.end()
+                }
+            }
+        }
+        if(noError){
+            let toWrite = '<video controls="" height="288" width="512"><source src="'+req.url+'src" type="video/mp4" />Your browser does not support the video tag.</video>'
+            res.writeHead(200, {"Content-Type":"text/html"})
+            res.write(toWrite)
+            res.end()
+        }
+    }
+    if(!res.writableEnded && req.url.endsWith(".mp4src")){ // Support for any.mp4
+        noError = true
+        try {
+            let urlFix = req.url.replace(".mp4src", ".mp4")
+            var file = fs.readFileSync("./html"+urlFix)
+        }catch(e){
+            if(e){
+                if(e.message.includes("no such file")){
+                    noError = false
+                    res.writeHead(404, {'Content-Type':'text/html'})
+                    res.write(fs.readFileSync("./assets/404.html"))
+                    res.end()
+                }
+            }
+        }
+        if(noError){
+            res.writeHead(200, {"Content-Type":"audio/basic"})
+            let urlFix = req.url.replace(".mp4src", ".mp4")
+            res.write(fs.readFileSync("./html"+urlFix))
+            res.end()
+        }
+    }
     // Check for unsupported file type
     if(!res.writableEnded && req.url.includes(".")){
         let fileType = req.url.split(".")[1]
